ccd0c9bc3e03142cf3b555923adc72930e4f20c6
Lunar-Scripts <lunar.cfxresources@gmail.com>
2024-02-25 13:23:14 +0100

    feat: finish config

--- config/config.lua
+++ config/config.lua
@@ -2,65 +2,157 @@ Config = {}
 
 ---@type false | string | string[]
 Config.requiredJob = false
+Config.progressPerCatch = 0.05 -- The progress per one fish caught
 
----@class Fish 
----@field name string
+---@class Fish
 ---@field price integer | { min: integer, max: integer }
 ---@field chance integer Percentage chance
 ---@field skillcheck SkillCheckDifficulity }
 
----@type Fish[]
+---@type table<string, Fish>
 Config.fish = {
-    { name = 'trout', price = { min = 20, max = 50 }, chance = 20, skillcheck = { 'easy', 'medium' } },
+    ['anchovy'] = { price = { min = 25, max = 50 }, chance = 35, skillcheck = { 'easy', 'medium' } },
+    ['trout'] = { price = { min = 50, max = 100 }, chance = 35, skillcheck = { 'easy', 'medium' } },
+    ['haddock'] = { price = { min = 150, max = 200 }, chance = 20, skillcheck = { 'easy', 'medium' } },
+    ['salmon'] = { price = { min = 200, max = 250 }, chance = 10, skillcheck = { 'easy', 'medium', 'medium' } },
+    ['grouper'] = { price = { min = 300, max = 350 }, chance = 25, skillcheck = { 'easy', 'medium', 'medium', 'medium' } },
+    ['piranha'] = { price = { min = 350, max = 450 }, chance = 25, skillcheck = { 'easy', 'medium', 'hard' } },
+    ['red_snapper'] = { price = { min = 400, max = 450 }, chance = 20, skillcheck = { 'easy', 'medium', 'medium', 'medium' } },
+    ['mahi_mahi'] = { price = { min = 450, max = 500 }, chance = 20, skillcheck = { 'easy', 'medium', 'medium', 'medium' } },
+    ['tuna'] = { price = { min = 1250, max = 1500 }, chance = 5, skillcheck = { 'easy', 'medium', 'hard' } },
+    ['shark'] = { price = { min = 2250, max = 2750 }, chance = 1, skillcheck = { 'easy', 'medium', 'hard' } },
 }
 
 ---@class FishingRod
 ---@field name string
 ---@field price integer
----@field level integer The minimal level
----@field chanceMultiplier number
+---@field minLevel integer The minimal level
 ---@field breakChance integer Percentage chance
 
 ---@type FishingRod[]
 Config.fishingRods = {
-    { name = 'normal_rod', price = 1000, level = 1, chanceMultiplier = 1.0, breakChance = 5 }
+    { name = 'basic_rod', price = 1000, minLevel = 1, breakChance = 20 },
+    { name = 'graphite_rod', price = 2500, minLevel = 2, breakChance = 10 },
+    { name = 'titanium_rod', price = 5000, minLevel = 3, breakChance = 1 },
+}
+
+---@class FishingBait
+---@field name string
+---@field price integer
+---@field minLevel integer The minimal level
+---@field waitDivisor number The total wait time gets divided by this value
+
+---@type FishingBait[]
+Config.baits = {
+    { name = 'worms', price = 5, minLevel = 1, waitDivisor = 1.0 },
+    { name = 'artificial_bait', price = 50, minLevel = 2, waitDivisor = 3.0 },
 }
 
 ---@class FishingZone
----@field coords vector3
+---@field locations vector3[] One of these gets picked at random
 ---@field radius number
 ---@field minLevel integer
+---@field waitTime { min: integer, max: integer }
 ---@field includeOutside boolean Whether you can also catch fish from Config.outside
 ---@field blip BlipData?
----@field fish string[]
+---@field message { enter: string, exit: string }?
+---@field fishList string[]
 
 ---@type FishingZone[]
 Config.fishingZones = {
     {
-        coords = vector3(0.0, 0.0, 0.0),
-        radius = 3.0,
+        blip = {
+            name = 'Coral Reef',
+            sprite = 317,
+            color = 24,
+            scale = 0.6
+        },
+        locations = {
+            vector3(-1774.0654, -1796.2740, 0.0),
+            vector3(2482.8589, -2575.6780, 0.0)
+        },
+        radius = 250.0,
         minLevel = 1,
+        waitTime = { min = 5, max = 10 },
         includeOutside = true,
-        fish = {}
-    }
+        message = { enter = 'You have entered a coral reef.', exit = 'You have left the coral reef.' },
+        fishList = { 'mahi_mahi', 'red_snapper' }
+    },
+    {
+        blip = {
+            name = 'Deep Waters',
+            sprite = 317,
+            color = 29,
+            scale = 0.6
+        },
+        locations = {
+            vector3(-4941.7964, -2411.9146, 0.0),
+        },
+        radius = 1000.0,
+        minLevel = 3,
+        waitTime = { min = 20, max = 40 },
+        includeOutside = false,
+        message = { enter = 'You have entered deep waters.', exit = 'You have left deep waters.' },
+        fishList = { 'grouper', 'tuna', 'shark' }
+    },
+    {
+        blip = {
+            name = 'Swamp',
+            sprite = 317,
+            color = 56,
+            scale = 0.6
+        },
+        locations = {
+            vector3(-2188.1182, 2596.9348, 0.0),
+        },
+        radius = 200.0,
+        minLevel = 2,
+        waitTime = { min = 10, max = 20 },
+        includeOutside = true,
+        message = { enter = 'You have entered a swamp.', exit = 'You have left the swamp.' },
+        fishList = { 'piranha' }
+    },
 }
 
 -- Outside of all zones
 Config.outside = {
-    chance = 25, -- The chance to catch a fish
+    waitTime = { min = 10, max = 25 },
 
     ---@type string[]
-    items = {
-        'trout', 'anchovy'
+    fishList = {
+        'trout', 'anchovy', 'haddock', 'salmon'
     }
 }
 
-
 Config.ped = {
     model = `s_m_m_cntrybar_01`,
+    buyAccount = 'money',
+    sellAccount = 'money',
+    blip = {
+        name = 'SeaTrade Corporation',
+        sprite = 356,
+        color = 74,
+        scale = 0.75
+    },
 
     ---@type vector4[]
     locations = {
+        vector4(-2081.3831, 2614.3223, 3.0840, 112.7910),
+        vector4(-1492.3639, -939.2579, 10.2140, 144.0305)
+    }
+}
 
+Config.renting = {
+    model = `s_m_m_dockwork_01`, -- The ped model
+    account = 'money',
+    boats = {
+        { model = `speeder`, price = 500, image = 'https://i.postimg.cc/mDSqWj4P/164px-Speeder.webp' },
+        { model = `dinghy`, price = 750, image = 'https://i.postimg.cc/ZKzjZgj0/164px-Dinghy2.webp'  },
+        { model = `tug`, price = 1250, image = 'https://i.postimg.cc/jq7vpKHG/164px-Tug.webp' }
+    },
+
+    ---@type { coords: vector4, spawn: vector4 }[]
+    locations = {
+        { coords = vector4(-1434.4818, -1512.2745, 2.1486, 25.8666), spawn = vector4(-1494.4496, -1537.6943, 2.3942, 115.6015) }
     }
 }
