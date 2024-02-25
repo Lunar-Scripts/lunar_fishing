0924eebf6fbe7c0f100e036fde7a5ee645c2d6ca
Lunar-Scripts <lunar.cfxresources@gmail.com>
2024-02-25 13:22:50 +0100

    feat(cl_edit): add ShowProgressBar

--- config/cl_edit.lua
+++ config/cl_edit.lua
@@ -23,6 +23,22 @@ function HideUI()
     lib.hideTextUI()
 end
 
+function ShowProgressBar(text, duration, canCancel, anim, prop)
+    return lib.progressBar({
+        duration = duration,
+        label = text,
+        useWhileDead = false,
+        canCancel = canCancel,
+        disable = {
+            car = true,
+            move = true,
+            combat = true
+        },
+        anim = anim,
+        prop = prop
+    })
+end
+
 function GetVehicleFuel(vehicle)
     if GetResourceState('LegacyFuel') == 'started' then
         local fuelLevel = exports['LegacyFuel']:GetFuel(vehicle, fuelLevel)
@@ -46,4 +62,36 @@ function SetVehicleOwner(plate)
     elseif Framework.name == 'qb-core' then
         TriggerEvent("vehiclekeys:client:SetOwner", plate)
     end
+end
+
+local function isStarted(resourceName)
+    return GetResourceState(resourceName) == 'started'
+end
+
+---@type string
+local path
+
+if isStarted('ox_inventory') then
+    path = 'nui://ox_inventory/web/images/%s.png'
+elseif isStarted('qb-inventory') then
+    path = 'nui://qb-inventory/html/images/%s.png'
+elseif isStarted('ps-inventory') then
+    path = 'nui://ps-inventory/html/images/%s.png'
+elseif isStarted('lj-inventory') then
+    path = 'nui://lj-inventory/html/images/%s.png'
+elseif isStarted('qs-inventory') then
+    path = 'nui://qs-inventory/html/images/%s.png' -- Not really sure
+end
+
+---Returns the NUI path of an icon.
+---@param itemName string
+---@return string?
+---@diagnostic disable-next-line: duplicate-set-field
+function GetInventoryIcon(itemName)
+    if not path then
+        warn('Inventory images path not set in cl_edit.lua!')
+        return
+    end
+
+    return path:format(itemName) .. '?height=128'
 end
