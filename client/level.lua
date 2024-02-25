b9918efdf53321a50442409a67ac77d27dda0845
Lunar-Scripts <lunar.cfxresources@gmail.com>
2024-02-25 13:20:04 +0100

    feat: level system

--- client/level.lua
+++ client/level.lua
@@ -1,7 +1,18 @@
+local level
+
+---@param l number
+local function updated(l)
+    level = l
+    Update(level)
+end
+
+lib.callback('lunar_fishing:getLevel', false, updated)
+RegisterNetEvent('lunar_fishing:updateLevel', updated)
+
 function GetCurrentLevel()
-    return 1
+    return math.floor(level)
 end
 
 function GetCurrentLevelProgress()
-    return 0.2
+    return level - math.floor(level)
 end
---
+++ server/level.lua
@@ -0,0 +1,70 @@
+---@type table<string, number>
+local levels = {}
+
+MySQL.ready(function()
+    local data = MySQL.query.await('SELECT * FROM lunar_fishing')
+
+    for _, entry in ipairs(data) do
+        levels[entry.identifier] = entry.xp
+    end
+end)
+
+local function save()
+    local query = 'UPDATE lunar_fishing SET xp = ? WHERE identifier = ?'
+    local parameters = {}
+    local size = 0
+
+    for identifier, level in pairs(levels) do
+        size += 1
+        parameters[size] = {
+            level,
+            identifier
+        }
+    end
+
+    if size > 0 then
+        print('Saving player progress.')
+        MySQL.prepare.await(query, parameters)
+    end
+end
+
+lib.cron.new('*/10 * * * *', save)
+
+local function createPlayer(identifier)
+    levels[identifier] = 1.0
+    MySQL.insert.await('INSERT INTO lunar_fishing (identifier, xp) VALUES(?, ?)', { identifier, levels[identifier] })
+end
+
+lib.callback.register('lunar_fishing:getLevel', function(source)
+    local player = Framework.getPlayerFromId(source)
+
+    if not player then return end
+
+    local identifier = player:getIdentifier()
+
+    if not levels[identifier] then
+        createPlayer(identifier)
+    end
+
+    return levels[identifier]
+end)
+
+---@param player Player
+---@param amount number
+function AddPlayerLevel(player, amount)
+    local identifier = player:getIdentifier()
+    local level = math.floor(levels[identifier])
+
+    levels[identifier] += amount
+
+    if math.floor(levels[identifier]) - level > 0 then
+        TriggerClientEvent('lunar_fishing:showNotification', player.source, locale('unlocked_level'), 'success')
+    end
+
+    TriggerClientEvent('lunar_fishing:updateLevel', levels[identifier])
+end
+
+---@param player Player
+function GetPlayerLevel(player)
+    return levels[player:getIdentifier()]
+end
